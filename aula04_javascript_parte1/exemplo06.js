617d32ec0eedeb3149971f7709ed2c7cfd4746e4
xXSamAndre <107582333+xXSamAndre@users.noreply.github.com>
2024-04-23 07:54:53 -0300

    Add files via upload

---
+++ aula04_javascript_parte1/exemplo06.js
@@ -0,0 +1,5 @@
+var nome = 'Luiz'
+var idade = 40
+var salvo = true
+var cidade;
+var tipo = typeof nome
---
+++ aula04_javascript_parte1/exemplo09.js
@@ -0,0 +1,5 @@
+var hora = 11
+
+if ((hora < 12)||(hora > 17)){
+    console.log('Voce esta na hora do rush')
+}
